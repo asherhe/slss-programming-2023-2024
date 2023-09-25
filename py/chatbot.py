e642bb0ff240ba19b2062d45e8bd43bff970e7c1
asherhe <github@asherhe.com>
2023-09-25 09:47:43 -0700

    add timing to chatbot

--- py/chatbot.py
+++ py/chatbot.py
@@ -3,22 +3,45 @@
 # date: 2023-09-20
 
 import random
+import time
+
+
+def typewriter(msg):
+    for c in msg:
+        print(c, end="")
+        if c in ".!?":
+            time.sleep(0.2)
+        elif c == ",":
+            time.sleep(0.1)
+        else:
+            time.sleep(0.05)
+
 
 # greet the user
 # get the user's name and store it in a variable
-name = input("hi! i'm a bot. what's your name? ")
+typewriter("hi! i'm a crude chatbot.\n")
+time.sleep(0.5)
+typewriter("what's your name? ")
+name = input()
+time.sleep(1)
+
 # respond with user's name
-print(f"it's good to meet you {name}!")
+typewriter(f"it's good to meet you, {name}!\n")
+time.sleep(1.5)
+
 # ask the user what their favorite food is
-food = input("what's your favorite food? ")
+typewriter("what's your favorite food? ")
+food = input()
+time.sleep(1)
+
 # make a NOT REPETITIVE comment about their food
 # create list of possible responses
 food_responses = [
-    f"yum, {food}!",
-    f"{food} is delicious!",
-    f"ew, {food} is disgusting",
-    f"i've never had {food} before",
+    f"yum, {food}!\n",
+    f"{food} is delicious!\n",
+    f"ew, {food} is disgusting!\n",
+    f"hmm, i've never had {food} before.\n",
 ]
 # choose a random one
 # print that one
-print(random.choice(food_responses))
+typewriter(random.choice(food_responses))
