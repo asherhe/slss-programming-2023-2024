beda8eeb3c79c38f0c6da7b048c03ee712289014
asherhe <github@asherhe.com>
2023-10-10 14:46:04 -0700

    add new year countdown

--- py/loop_practice.py
+++ py/loop_practice.py
@@ -2,6 +2,8 @@
 # author: asher
 # date: 2023-10-10
 
+import time
+
 # create a list of groceries
 groceries = ["hot wheels", "lego", "ice cream", "video games"]
 
@@ -18,6 +20,12 @@ for item in groceries:
     print(f"# {item}\n  ---")
 
 # print pyramid thing
-
 for i in range(6):
     print("*" * (i + 1))
+
+# new year countdown
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+
+print("HAPPY NEW YEAR")
