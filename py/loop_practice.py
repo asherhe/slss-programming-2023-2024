24f77c3e6d5626be23d265f37b3be1a75f53cf38
asherhe <github@asherhe.com>
2023-10-13 08:28:46 -0700

    implement linear search

--- py/loop_practice.py
+++ py/loop_practice.py
@@ -26,6 +26,23 @@ for i in range(6):
 # new year countdown
 for i in range(10, 0, -1):
     print(i)
-    time.sleep(1)
+    # time.sleep(1)
 
 print("HAPPY NEW YEAR")
+
+# implement linear search
+names = []
+with open("./names.txt", "r") as file:
+    names = map(lambda s: s.strip("\n"), file.readlines())
+search_name = "Deanna Lee"
+
+for name in names:
+    if name == search_name:
+        print(f"We found {search_name}!")
+        break
+else:
+    print(f"We didn't find {search_name}")
+
+# print out "Mr. Ubial is pretty cool" 20 times
+for _ in range(20):
+    print("Mr. Ubial is pretty cool")
