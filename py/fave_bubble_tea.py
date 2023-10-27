808080ed03a1c6f9433a366d4fdcdbdaf5f4d754
asherhe <github@asherhe.com>
2023-10-27 09:43:30 -0700

    add comments

--- py/fave_bubble_tea.py
+++ py/fave_bubble_tea.py
@@ -8,15 +8,15 @@
 # CONSTANTS
 NUM_RESPONDENTS = 5 # number of respondents
 
+# keep track of a tally for each option
 like_count = {}
 
+# repeat a number of times
 for _ in range(NUM_RESPONDENTS):
   # ask the user where their favorite place is
-  # store it somewhere
   fave_place = input("what's your favorite bubble tea place? ").strip(".,!?").lower()
-  
-  # tally
-  # increment a counter for each bubble tea place
+
+  # store it somewhere  
   if fave_place in like_count:
     like_count[fave_place] += 1
   else:
@@ -25,4 +25,5 @@ for _ in range(NUM_RESPONDENTS):
 # print a summary
 print("\nsummary\n=======")
 for place in sorted(like_count, key=like_count.get, reverse=True):
+  # include place, count, and percent
   print(f"{place}: {like_count[place]} ({like_count[place] * 100 // NUM_RESPONDENTS}%)")
