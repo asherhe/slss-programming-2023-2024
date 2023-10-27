90dd1d8140f7b050a688f60acbceee35d0f8d811
asherhe <github@asherhe.com>
2023-10-27 09:08:30 -0700

    add number practice

---
+++ py/number_practice.py
@@ -0,0 +1,17 @@
+# number practic
+# asher
+# 2023-10-27
+
+# operators with numbers
+print(1 + 1)
+print(1 - 1)
+print(10 * 10)
+print(9 / 3)
+
+print(1 + 2 / 4 + 5 - 4) # big expression
+
+print(9 / 2) # normal division
+print(9 // 2) # floor division
+print(9 % 2) # modulo
+
+print(2 ** 2) # exponent
