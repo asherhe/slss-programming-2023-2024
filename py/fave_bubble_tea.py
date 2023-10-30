5bff4b96da295e7bc1bde3096ed9d821519a3a9f
asherhe <github@asherhe.com>
2023-10-30 09:06:47 -0700

    put non-bubble tea places into the "other" category

--- py/fave_bubble_tea.py
+++ py/fave_bubble_tea.py
@@ -7,16 +7,20 @@
 
 # CONSTANTS
 NUM_RESPONDENTS = 5 # number of respondents
+BUBBLE_TEA_PLACES = ["coco", "suntea", "chatime", "bubble queen"]
 
 # keep track of a tally for each option
 like_count = {}
 
 # repeat a number of times
-for _ in range(NUM_RESPONDENTS):
+for i in range(1, NUM_RESPONDENTS + 1):
   # ask the user where their favorite place is
-  fave_place = input("what's your favorite bubble tea place? ").strip(".,!?").lower()
+  fave_place = input(f"user {i}, what's your favorite bubble tea place? ").strip(".,!?").lower()
+  
+  if fave_place not in BUBBLE_TEA_PLACES:
+    fave_place = "other"
 
-  # store it somewhere  
+  # store it somewhere
   if fave_place in like_count:
     like_count[fave_place] += 1
   else:
@@ -25,5 +29,6 @@ for _ in range(NUM_RESPONDENTS):
 # print a summary
 print("\nsummary\n=======")
 for place in sorted(like_count, key=like_count.get, reverse=True):
-  # include place, count, and percent
-  print(f"{place}: {like_count[place]} ({like_count[place] * 100 // NUM_RESPONDENTS}%)")
+  # include name, count, and percent
+  like_percentage = round(like_count[place] * 100 / NUM_RESPONDENTS, 2)
+  print(f"{place}: {like_count[place]} ({like_percentage:.2f}%)")
